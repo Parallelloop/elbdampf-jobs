0c4bffac0a56a7000fd5024cf895f94b61e3f5bb
Nabeel Sajid <nabeelsajid917@gmail.com>
2026-01-07 21:10:26 +0500

    order test

--- src/services/sp-api/orders/order.js
+++ src/services/sp-api/orders/order.js
@@ -37,6 +37,7 @@ const fetchAmazonFBMOrdersPage = async ({ nextToken, lastUpdatedAfter }) => {
         MarketplaceIds: ['A1PA6795UKMFR9'],
         LastUpdatedAfter: lastUpdatedAfter,
         FulfillmentChannels: ['MFN'],
+        AmazonOrderIds: ["302-5138615-0169139"],
         MaxResultsPerPage: 100,
       };
     }
